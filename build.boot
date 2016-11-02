2029027289a7e13b0ff14fe25457c7669756850c
anatoly <tolitius@gmail.com>
2016-11-01 23:28:25 -0400

    welcome envoy

---
+++ build.boot
@@ -0,0 +1,25 @@
+(def +version+ "0.0.1")
+
+(set-env!
+  :source-paths #{"src"}
+  :dependencies '[[cheshire "5.6.3"]
+                  [org.clojure/core.async "0.2.395"]
+                  [http-kit "2.2.0"]
+
+                  ;; boot clj
+                  [boot/core              "2.6.0"           :scope "provided"]
+                  [adzerk/bootlaces       "0.1.13"          :scope "test"]])
+
+(require '[adzerk.bootlaces :refer :all])
+
+(bootlaces! +version+)
+
+(task-options!
+  push {:ensure-branch nil}
+  pom {:project     'tolitius/envoy
+       :version     +version+
+       :description "a gentle touch of clojure to hashicorp's consul"
+       :url         "https://github.com/tolitius/envoy"
+       :scm         {:url "https://github.com/tolitius/envoy"}
+       :license     {"Eclipse Public License"
+                     "http://www.eclipse.org/legal/epl-v10.html"}})
---
+++ src/envoy/core.clj
@@ -0,0 +1,62 @@
+(ns envoy.core
+  (:require [cheshire.core :as json]
+            [clojure.core.async :refer [go-loop go <! >! >!! alt! chan]]
+            [org.httpkit.client :as http])
+  (:import [javax.xml.bind DatatypeConverter]))
+
+(defn- recurse [path]
+  (str path "?recurse"))
+
+(defn- index-of [resp]
+  (-> resp
+      :headers
+      :x-consul-index))
+
+(defn- read-index [path]
+  (-> (http/get path)
+      index-of))
+
+(defn- fromBase64 [s]
+  (String. (DatatypeConverter/parseBase64Binary s)))
+
+(defn- read-values [{:keys [body]}]
+  (into {}
+    (for [{:keys [Key Value]} (json/parse-string body true)]
+      [(keyword Key) 
+       (when Value (fromBase64 Value))])))
+
+(defn put [path v]
+  @(http/put path {:body v}))
+
+(defn delete [path]
+  @(http/delete (recurse path)))
+
+(defn get-all [path]
+  (-> @(http/get (recurse path))
+      read-values))
+
+(defn- start-watcher [path fun stop?]
+  (let [ch (chan)]
+    (go-loop [index (read-index path)]
+      (http/get path
+                {:query-params {:index index}}
+                #(>!! ch %))
+      (alt!
+        stop? ([_]
+               (prn "stopping" path "watcher"))
+        ch ([resp] 
+            (fun (read-values resp))
+            (recur (index-of resp)))))))
+
+(defprotocol Stoppable
+  (stop [this]))
+
+(deftype Watcher [ch]
+  Stoppable
+  (stop [_]
+    (>!! ch :done)))
+
+(defn watch-path [path fun]
+  (let [stop-ch (chan)]
+    (start-watcher path fun stop-ch)
+    (Watcher. stop-ch)))
